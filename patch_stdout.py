921b0d4b834c0ae1617f366726e216cf894a8ba0
Aluriak <lucas.bourneuf@laposte.net>
2021-03-09 20:28:37 +0100

    add stdout+accumulator implementation of patch stdout

--- patch_stdout.py
+++ patch_stdout.py
@@ -5,3 +5,22 @@ output = StringIO()
 with redirect_stdout(output):
     print('hello, world!', end='')
 assert output.getvalue() == 'hello, world!'
+
+
+
+class PrintAndAcc():
+    "Replacement for io.StringIO() instance where stdout is both accumulated and printed in real time in stdout"
+    def __init__(self, target=sys.stdout):
+        self.content = []
+        self.target = target
+    def write(self, content):
+        print(content, end='', file=self.target)
+        self.content.append(content)  # avoid O2 complexity
+    def getvalue(self):
+        return ''.join(self.content)
+
+acc = PrintAndAcc()
+with contextlib.redirect_stdout(acc):
+    print('test')
+assert acc.getvalue() == 'test'
+
